51856046e803400d49b5d43c61ca99b11763874c
ka <624235036@parichat.skru.ac.th>
2021-10-03 17:08:48 +0700

    add books

---
+++ src/components/BookItem.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="book-item">
+        <h1>{{book.title}}</h1><br/>
+        <img v-bind:src="book.thumbnailUrl" /><br/>
+        <b>author:</b> {{book.author}} <br/>
+        <b>category:</b> {{book.category}} <br/>
+        <b>ISBN:</b> {{book.isbn}} <br/>
+        <b>pages:</b> {{book.pageCount}} <br/><br/>
+        <b>Description:</b><br/>
+        <p>{{book.shortDescription}}</p>
+    </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'BookItem',
+  components: {
+
+  },
+    props:["book"]
+}
+</script>
+
+<style>
+.book-item{
+    width: 70%;
+    background: #f4f4ff;
+    padding: 10px;
+    border-bottom: 1px #ccc dotted;
+}
+</style>
